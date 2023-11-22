8f585d9d6440a25b822d1d30bc5af50a52ed5843
Orpheus Lummis <o@orpheuslummis.info>
2023-11-22 18:39:04 +0300

    add anonymous feedback form

--- src/navigation.js
+++ src/navigation.js
@@ -2,118 +2,118 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-  //   {
-  //     text: 'Homes',
-  //     links: [
-  //       {
-  //         text: 'SaaS',
-  //         href: getPermalink('/homes/saas'),
-  //       },
-  //       {
-  //         text: 'Startup',
-  //         href: getPermalink('/homes/startup'),
-  //       },
-  //       {
-  //         text: 'Mobile App',
-  //         href: getPermalink('/homes/mobile-app'),
-  //       },
-  //       {
-  //         text: 'Personal',
-  //         href: getPermalink('/homes/personal'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Pages',
-  //     links: [
-  //       {
-  //         text: 'Features (Anchor Link)',
-  //         href: getPermalink('/#features'),
-  //       },
-  //       {
-  //         text: 'Services',
-  //         href: getPermalink('/services'),
-  //       },
-  //       {
-  //         text: 'Pricing',
-  //         href: getPermalink('/pricing'),
-  //       },
-  //       {
-  //         text: 'About us',
-  //         href: getPermalink('/about'),
-  //       },
-  //       {
-  //         text: 'Contact',
-  //         href: getPermalink('/contact'),
-  //       },
-  //       {
-  //         text: 'Terms',
-  //         href: getPermalink('/terms'),
-  //       },
-  //       {
-  //         text: 'Privacy policy',
-  //         href: getPermalink('/privacy'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Landing',
-  //     links: [
-  //       {
-  //         text: 'Lead Generation',
-  //         href: getPermalink('/landing/lead-generation'),
-  //       },
-  //       {
-  //         text: 'Long-form Sales',
-  //         href: getPermalink('/landing/sales'),
-  //       },
-  //       {
-  //         text: 'Click-Through',
-  //         href: getPermalink('/landing/click-through'),
-  //       },
-  //       {
-  //         text: 'Product Details (or Services)',
-  //         href: getPermalink('/landing/product'),
-  //       },
-  //       {
-  //         text: 'Coming Soon or Pre-Launch',
-  //         href: getPermalink('/landing/pre-launch'),
-  //       },
-  //       {
-  //         text: 'Subscription',
-  //         href: getPermalink('/landing/subscription'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Blog',
-  //     links: [
-  //       {
-  //         text: 'Blog List',
-  //         href: getBlogPermalink(),
-  //       },
-  //       {
-  //         text: 'Article',
-  //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-  //       },
-  //       {
-  //         text: 'Article (with MDX)',
-  //         href: getPermalink('markdown-elements-demo-post', 'post'),
-  //       },
-  //       {
-  //         text: 'Category Page',
-  //         href: getPermalink('tutorials', 'category'),
-  //       },
-  //       {
-  //         text: 'Tag Page',
-  //         href: getPermalink('astro', 'tag'),
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     text: 'Widgets',
-  //     href: '#',
-  //   },
+    //   {
+    //     text: 'Homes',
+    //     links: [
+    //       {
+    //         text: 'SaaS',
+    //         href: getPermalink('/homes/saas'),
+    //       },
+    //       {
+    //         text: 'Startup',
+    //         href: getPermalink('/homes/startup'),
+    //       },
+    //       {
+    //         text: 'Mobile App',
+    //         href: getPermalink('/homes/mobile-app'),
+    //       },
+    //       {
+    //         text: 'Personal',
+    //         href: getPermalink('/homes/personal'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     text: 'Pages',
+    //     links: [
+    //       {
+    //         text: 'Features (Anchor Link)',
+    //         href: getPermalink('/#features'),
+    //       },
+    //       {
+    //         text: 'Services',
+    //         href: getPermalink('/services'),
+    //       },
+    //       {
+    //         text: 'Pricing',
+    //         href: getPermalink('/pricing'),
+    //       },
+    //       {
+    //         text: 'About us',
+    //         href: getPermalink('/about'),
+    //       },
+    //       {
+    //         text: 'Contact',
+    //         href: getPermalink('/contact'),
+    //       },
+    //       {
+    //         text: 'Terms',
+    //         href: getPermalink('/terms'),
+    //       },
+    //       {
+    //         text: 'Privacy policy',
+    //         href: getPermalink('/privacy'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     text: 'Landing',
+    //     links: [
+    //       {
+    //         text: 'Lead Generation',
+    //         href: getPermalink('/landing/lead-generation'),
+    //       },
+    //       {
+    //         text: 'Long-form Sales',
+    //         href: getPermalink('/landing/sales'),
+    //       },
+    //       {
+    //         text: 'Click-Through',
+    //         href: getPermalink('/landing/click-through'),
+    //       },
+    //       {
+    //         text: 'Product Details (or Services)',
+    //         href: getPermalink('/landing/product'),
+    //       },
+    //       {
+    //         text: 'Coming Soon or Pre-Launch',
+    //         href: getPermalink('/landing/pre-launch'),
+    //       },
+    //       {
+    //         text: 'Subscription',
+    //         href: getPermalink('/landing/subscription'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     text: 'Blog',
+    //     links: [
+    //       {
+    //         text: 'Blog List',
+    //         href: getBlogPermalink(),
+    //       },
+    //       {
+    //         text: 'Article',
+    //         href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //       },
+    //       {
+    //         text: 'Article (with MDX)',
+    //         href: getPermalink('markdown-elements-demo-post', 'post'),
+    //       },
+    //       {
+    //         text: 'Category Page',
+    //         href: getPermalink('tutorials', 'category'),
+    //       },
+    //       {
+    //         text: 'Tag Page',
+    //         href: getPermalink('astro', 'tag'),
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     text: 'Widgets',
+    //     href: '#',
+    //   },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
@@ -126,6 +126,10 @@ export const footerData = {
         { text: 'Email', href: 'mailto:team@horizonevents.info' },
         { text: 'Matrix', href: 'https://matrix.to/#/#horizonevents:matrix.org' },
         { text: 'Matrix (encrypted)', href: 'https://matrix.to/#/#orpheus@beeper.com' },
+        {
+          text: 'Anonymous feedback',
+          href: 'https://docs.google.com/forms/d/e/1FAIpQLSeQ9e_mGbPH9-YpFaxDxobXZpET4D5nhMigPdjdTXuSXRd6ww/viewform?usp=sf_link',
+        },
       ],
     },
     {
